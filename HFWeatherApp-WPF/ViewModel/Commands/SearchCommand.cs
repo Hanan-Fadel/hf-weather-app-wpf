34529254f89fb48912661bea806173a12e8794cd
Hanan Fadel <fadel_hanan@yahoo.com>
2022-03-22 21:14:25 -0600

    first commit

---
+++ HFWeatherApp-WPF/ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HFWeatherApp_WPF.ViewModel.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherVM VM { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value;  }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public SearchCommand(WeatherVM vm)
+        {
+            VM = vm;
+        }
+       
+        public bool CanExecute(object parameter)
+        {
+             string query = parameter as string;
+
+            if (string.IsNullOrWhiteSpace(query))
+                return false;
+            return true;
+
+        }
+
+        //This method will be executed if the canExecute returned true
+        public void Execute(object parameter)
+        {
+            VM.makeQuery();
+        }
+    }
+}
---
+++ HFWeatherApp-WPF/ViewModel/WeatherVM.cs
@@ -0,0 +1,145 @@
+﻿using HFWeatherApp_WPF.Model;
+using HFWeatherApp_WPF.ViewModel.Commands;
+using HFWeatherApp_WPF.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFWeatherApp_WPF.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged       
+    {
+
+        //Constructor
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units()
+                        {
+                            Value = "21"
+                        }
+                    }
+
+                };
+
+            }
+
+            SearchCommand = new SearchCommand(this);
+
+            //Observable list have to be initialized only once otherwise any
+            //binding will be lost.
+            Cities = new ObservableCollection<City>();
+
+        }
+
+        //Declare the properties 
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value;
+
+                //In the setter we need to trigger the event PropertyChanged, we can trigger it by
+                //calling the OnPropertyChanged Method & passing the name of the peoperty that is changed 
+                OnPropertyChanged("Query"); 
+
+                //This event now is going to update anyone who is subscriping to 
+                //letting them know that this property has changed.
+                //This could  be possible because in the xaml file the TextBlocks for these proprties
+                //Can subscripe to that property so that whenever the value of the property
+                //changes, the text inside these textblocks changes as well.
+
+                //Also when the text inside these textbox changes, that is going to be  updateing 
+                //the Query that is can be used to call the API Query property
+            }
+        }
+
+        // Properties 
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set 
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+                GetCurrentConditions();
+            }
+        }
+
+
+        public ObservableCollection<City> Cities { get; set; }
+        public SearchCommand SearchCommand { get; set; }
+
+
+        private async void GetCurrentConditions()
+        {
+            //Clear the query as the user already choose the city
+            Query = string.Empty;
+
+            //Clear the cities
+            Cities.Clear();
+
+             CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+
+        }
+        public async void makeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            //before adding the cities to the observable list
+            //make sure to clear anyvalue inside this collection
+            Cities.Clear();
+            
+            foreach(var city in cities)
+            {
+                Cities.Add(city);
+            }
+
+           
+        }
+
+        //Trigger this event when these properties change
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        //Create a method to trigget the event Property changed
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
